e908efeb46b8e3c87ba5f4c07a0464d707732762
SamVanderstraeten <31991068+SamVanderstraeten@users.noreply.github.com>
2018-12-05 11:00:24 +0100

    Cleaning up

--- day5.py
+++ day5.py
@@ -1,16 +1,14 @@
 file = open("input.txt", "r")
 
-#9704
-
 lines = file.readlines()
 l = lines[0]
-
-
 
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 best = 10000000
 bestic = '.'
 for ic in chars:
+
+    print("Calculating score for " + ic.upper())
 
     line = []
     for c in l:
@@ -18,10 +16,7 @@ for ic in chars:
             line.append(c)
 
     i = 0
-    imax = 0
     while i < len(line):
-        #print("i: " + str(i))
-
         char = line[i]
         if char == '.':
             i += 1
@@ -34,37 +29,26 @@ for ic in chars:
                     nextcharj = j
                     break
             if not (nextchar == '&'): # ended
-                #print(char, nextchar, char == nextchar, char.upper() == nextchar.upper())
                 if (not (char == nextchar)) and (char.upper() == nextchar.upper()):
                     line[i] = '.'
                     line[nextcharj] = '.'
-                    
-                    #print(line)
                     while line[i] == '.' and i > 0:
                         i -= 1
-                    '''if i == 0 and line[i] == '.':
-                        for x in range(0, len(line)):
-                            if line[x] == '.':
-                                x += 1
-                        i = x'''
                 else:
                     i += 1
-                    #print("Resume: " + str(i))
             else:
                 i += 1
-            
-            if i > imax:
-                imax = i
-                print(ic + ">> " + str(imax) + "/" + str(len(line)))
-                #print(line)
 
     count = 0
     for c in line:
         if not c == '.':
             count += 1
 
+    print("Score: " + str(count))
+
     if count < best:
         best = count
         bestic = ic
+        print("Best so far!")
 
 print(str(best) + ", " + bestic)
