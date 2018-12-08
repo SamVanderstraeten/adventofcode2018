bb85b389d04ae39f2e8483d4c5dac2970ad9db15
SamVanderstraeten <info@awesam.be>
2018-12-08 13:21:24 +0100

    day8.2

--- day8.py
+++ day8.py
@@ -9,15 +9,20 @@ def parseNode(i=0):
     numChilds = int(numbers[i])
     numMeta = int(numbers[i+1])
     currentChild = i+2
+    nodeValue = 0
+    childNodeValue = 0
+    childNodeValues = {}
     for n in range(0, numChilds):
-        currentChild += parseNode(currentChild)
+        (childNodeValue, currentChildDelta) = parseNode(currentChild)
+        currentChild += currentChildDelta
+        childNodeValues[(n+1)] = childNodeValue
     for m in range(0, numMeta):
-        metas.append(int(numbers[currentChild+m]))
-    return currentChild-i+numMeta
+        if numChilds == 0:
+            nodeValue += int(numbers[currentChild+m])
+        elif int(numbers[currentChild+m]) <= numChilds:
+            nodeValue += childNodeValues[int(numbers[currentChild+m])]
+    return (nodeValue, currentChild-i+numMeta)
 
-metasum = 0
-parseNode()
-for m in metas:
-    metasum += m
-print(str(metasum))
+(value, count) = parseNode()
+print(str(value))
 
