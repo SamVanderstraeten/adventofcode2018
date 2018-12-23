5497f29659fae21c5d3ec9d38df6dc895cd54109
SamVanderstraeten <info@awesam.be>
2018-12-23 21:52:15 +0100

    day 23 part 2

--- day22.py
+++ day22.py
@@ -6,8 +6,8 @@ depth = 3558
 #target = (10+1,10+1)
 #depth = 510
 
-grid = np.zeros([target[0],target[1]], dtype=int)
-groundmap = np.zeros([target[0], target[1]], dtype=int)
+grid = np.zeros([target[0]*2,target[1]*2], dtype=int)
+groundmap = np.zeros([target[0]*2, target[1]*2], dtype=int)
 
 risk = 0
 
@@ -36,9 +36,37 @@ for row in range(0,target[0]):
         riskfactor = erosionlevel % 3
         groundmap[row][col] = riskfactor
 
-        risk += riskfactor
+        if row < target[0] and col < target[1]:
+            risk += riskfactor
 
 printMap()
 print("Risk level: " + str(risk))
 
+def unvisited_neighbors(currentNode):
+    nbs = []
+    for r in range(-1,2):
+        for c in range(-1,2):
+            if r == 0 and c == 0:
+                continue
+            elif r == 0 or c == 0 and currentNode[0] + r >= 0 and currentNode[1] + c >= 0 and currentNode[0] + r < len(visited) and currentNode[1] + c < len(visited[0]):
+                if visited[currentNode[0] + r][currentNode[1] + c] == 0:
+                    nbs.append([r,c])
+
+def get_cost(fromNode, toNode):
+
+
 # dijkstra time
+costgrid = np.zeros([target[0]*2, target[1]*2], dtype=int)
+visited = np.zeros([target[0]*2, target[1]*2], dtype=int)
+current = [0,0]
+# keep current equipment
+
+for r in range(0,len(costgrid)):
+    for c in range(0, len(costgrid[r])):
+        costgrid[r][c] = 999999
+costgrid[current[0]][current[1]] = 0
+
+# select unvisited neighbor with smallest distance
+nbs = unvisited_neighbors(current)
+for nb in nbs:
+    cost = get_cost(current, nb)
